4245d3037a695536191e5370e88c58692296e7e0
Marc <markxvii@outlook.com>
2019-06-18 15:30:20 +0800

    针对首页列表开发做了少量更改，引入mintui

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,11 +6,14 @@
                 <span>{{address}}</span>
                 <i class="fa fa-sort-desc"></i>
             </div>
+        </div>
+        <div class="search_wrap">
             <div class="shop_search">
                 <i class="fa fa-search"></i>
                 搜索商家 商家名称
             </div>
         </div>
+        <div id="container" style="height: 2000px"></div>
     </div>
 </template>
 
@@ -35,9 +38,9 @@
   overflow: auto;
   box-sizing: border-box;
 }
-.header {
+.header,.search_wrap {
   background-color: #009eef;
-  padding: 16px;
+  padding:10px 16px;
 }
 .header .address_map {
   color: #fff;
@@ -54,12 +57,18 @@
   white-space: nowrap;
   text-overflow: ellipsis;
 }
-.header .shop_search {
-  margin-top: 10px;
+.search_wrap .shop_search {
   background-color: #fff;
   padding: 10px 0;
   border-radius: 4px;
   text-align: center;
   color: #aaa;
+}
+.search_wrap {
+    position: sticky;
+    top: 0px;
+    z-index: 999;
+    box-sizing: border-box;
+
 }
 </style>
